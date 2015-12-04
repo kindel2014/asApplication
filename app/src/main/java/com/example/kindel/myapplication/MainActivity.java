590aa82bd0cfc17c8242b66385f4c01c9b84cd24
kindel2014 <703936285@qq.com>
2015-12-04 14:31:37 +0800

    toast

--- app/src/main/java/com/example/kindel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kindel/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast.makeText(MainActivity.this,"aa",Toast.LENGTH_SHORT).show();
             }
         });
     }
